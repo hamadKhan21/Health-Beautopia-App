0c25d9958bc6cd1d437339165a59430373b4705c
hamad2k9 <hamad2k9@outlook.com>
2022-04-04 02:06:12 +0300

    Done

--- Beautopia.web/Areas/Admin/Controllers/ServicesLeadsController.cs
+++ Beautopia.web/Areas/Admin/Controllers/ServicesLeadsController.cs
@@ -41,7 +41,9 @@ namespace Beautopia.web.Areas.Admin.Controllers
 
 		public JsonResult GetRequestServices()
 		{
-			var data = _serviceRequest.GetRequestService("");
+			var login = HttpContext.Session.GetObjectFromJson<UserLogin>("Login");
+
+			var data = _serviceRequest.GetRequestServiceIfActivity(login.UserName,login.RoleID);
 
 
 			return Json(data);
